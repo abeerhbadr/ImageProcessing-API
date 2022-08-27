97ce0a7da6be5adc5b7d30b224ff5ee881cea0dd
Abeerhbadr <abeer.h.badr@gmail.com>
2022-08-28 01:58:07 +0200

    Image Resize

--- src/controllers/imageResizing.controller.ts
+++ src/controllers/imageResizing.controller.ts
@@ -1,6 +1,23 @@
 import { NextFunction, Request, Response } from 'express';
 import sharp from 'sharp';
 
-const imageResizing = (req: Request, res: Response, next: NextFunction) => {};
+const resizeImage = async (req: Request, res: Response, next: NextFunction) => {
+  const { filename, width, height } = req.query;
 
-export default imageResizing;
+  const fname: string = filename as string;
+  const w: number | null = width ? parseInt(width as string, 10) : null;
+  const h: number | null = height ? parseInt(width as string, 10) : null;
+  const outputFile: string = '';
+
+  try {
+    const resize = sharp(fname).resize({ w, h }).toFormat('jpg');
+
+    await resize.toFile(outputFile).then(()=>{
+        console.log('Image is Resized');
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default resizeImage;
